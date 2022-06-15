b571ded2c2b8a76c2626eefc9ac658e00300f45b
shekhar2922 <94707859+shekhar2922@users.noreply.github.com>
2022-06-15 22:46:28 +0530

    Update index.jsx

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import Image from "next/image";
-import Head from "next/Head";
+import Head from "next/head";
 import { useState } from "react";
 import styles from "../../styles/Admin.module.css";
 
