95f63cb9feaeace3c60421e058729df9abbe8f95
shekhar2922 <94707859+shekhar2922@users.noreply.github.com>
2022-06-15 22:46:44 +0530

    Update login.jsx

--- pages/admin/login.jsx
+++ pages/admin/login.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import Head from "next/Head";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import styles from "../../styles/Login.module.css";
