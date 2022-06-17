6d97ee2008039dc39e11b37f0b9dc8c267ff7e9c
Shekhar Chauhan <94707859+shekhar2922@users.noreply.github.com>
2022-06-17 13:00:05 +0530

    Update PizzaList.jsx

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -4,11 +4,9 @@ import PizzaCard from "./PizzaCard";
 const PizzaList = ({ pizzaList }) => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <h1 className={styles.title}>THE BEST PIZZA IN YOUR TOWN</h1>
       <p className={styles.desc}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut blandit arcu
-        in pretium molestie. Interdum et malesuada fames acme. Lorem ipsum dolor
-        sit amet, consectetur adipiscing elit.
+        Always our best food, made especially for you. All you need is love and Pizzaaa.
       </p>
       <div className={styles.wrapper}>
         {pizzaList.map((pizza) => (
